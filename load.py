9b0f468e7315b0067ab4dcf6417f03dfcd1c3e17
em0sh <em0sh@icloud.com>
2020-12-20 16:51:05 -0500

    feed forward and first layer definition working

--- load.py
+++ load.py
@@ -3,7 +3,7 @@ import numpy as np
 import struct, os, time, pickle
 from array import array
 
-# Library specifically designed for handling NMIST IDX
+# Library specifically designed for handling IDX
 import idx2numpy
 
 imgsize = 28
@@ -12,51 +12,53 @@ labelfile = 'data/trainlabel'
 
 # Reading
 ndarrc = idx2numpy.convert_from_file(imagefile)
-ndarr = np.zeros((60000, 784))
+ndarrl = len(ndarrc)
 nd = []
 # TODO: Comment this code
-for i in ndarrc[:500]:
+for i in ndarrc:
 	nd.append(np.concatenate(i, axis=None))
-# Load data
-with open('data/trainfile', 'rb') as f:
-	d = pickle.load(f)
-print(d)
 
-class Figure():
-    # Class for figure manipulation
+# Retrieve label data
+with open(labelfile, 'rb') as file:
+	magic, size = struct.unpack(">II", file.read(8))
+	if magic != 2049:
+		raise ValueError('2049 was magic, not {}'.format(magic))
+	l = array("B", file.read())		
 
-    def plot(f):
-    # Figure plotting
-        # string to build
-        s = ''
+################ Plotting
+def plot(f):
+# Figure plotting
+	# string to build
+	s = ''
 
-        for i in range(len(f)):
-            for j in range(len(f[i])):
-                if f[i][j] < 61:
-                    s += ' '
-                elif f[i][j] > 60 and f[i][j] < 190:
-                    s += '.'
-                else:
-                    s += 'x'
-            #    s += str(f[i][j])
-            print(str(s))
-            s = ''
+	k = 0
+	for i in range(imgsize):
+		for j in range(imgsize):
+			if f[j + k*imgsize] < 61:
+				s += ' '
+			elif f[j + k*imgsize] > 60 and f[i] < 190:
+				s += '.'
+			else:
+				s += 'x'
+		print(str(s))
+		k += 1
+		s = ''
 
-    def grayscale(f):
+def grayscale(f):
+	for i in range(len(f)):
+		for j in range(len(f[i])):
+			if f[i][j] < 61:
+				f[i][j] = 0
+			elif f[i][j] > 60 and f[i][j] < 190:
+				f[i][j] = 128
+			else:
+				f[i][j] = 255
+	return(f)
 
-        for i in range(len(f)):
-            for j in range(len(f[i])):
-                if f[i][j] < 61:
-                    f[i][j] = 0
-                elif f[i][j] > 60 and f[i][j] < 190:
-                    f[i][j] = 128
-                else:
-                    f[i][j] = 255
-        return(f)
 
-# Copy to array to work with
-gsImages = ndarr.copy()
 
+
+'''
 def label():		
 	l = []
 	with open(labelfile, 'rb') as file:
@@ -65,20 +67,15 @@ def label():
 			raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
 		l = array("B", file.read())		
 	return l
-
-
-# TODO: Comment this code
-def getData():
-	idata = []
-	ldata = label()
-	for i in range(len(gsImages)):
-		idata.append(Figure.grayscale(gsImages[i]))
-		#Figure.plot(idata[i])
-		print(i)
-	dataz = list(zip(idata, ldata)) 
-	return dataz
-
+'''
 	
+'''
 def test(lay):
 	a = gsImages[lay]
 	return(a)
+'''
+'''
+# Load data
+with open('data/trainfile', 'rb') as f:
+	d = pickle.load(f)
+'''
--- run.py
+++ run.py
@@ -5,10 +5,12 @@ n1 = net.Net([28*28, 30, 10], 10, .1)
 
 n1.initialize()
 
+t = 59999
 
-n1.l[0] = load.nd[0]
+n1.l[0] = load.nd[t]
 
-print(n1.l[0])
+load.plot(list(n1.l[0]))
+print(load.l[t])
 
 train.feed(n1)
 
